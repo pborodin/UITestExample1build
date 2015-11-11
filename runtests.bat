affe500ff13c47e6aaf6e60adc877372cc393b20
pborodin <borodin116@gmail.com>
2015-11-11 14:27:08 +0300

    Delete runtests.bat

--- runtests.bat
+++
@@ -1,1 +0,0 @@
-"c:\Users\hp\Programs\ReadyAPI-1.4.1\bin\testrunner.bat" -r -a -j -ftarget\surefire-reports -R"Project Report" -E"Default environment" -I -tC:\Users\hp\soapui-settings.xml soapui.xml
